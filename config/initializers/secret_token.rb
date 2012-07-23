d002033abb1d100641704b5747cc45ba9526967f
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2012-07-23 13:32:15 -0300

    depot work - continuing from strach

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '3a7f8d47024aee3fb3d1866b4537525d8be4bb3893ca163d7aaf9e5552f4959b807f2eb6c8bc8083f0b530168f879a6f209b7ae07a9fba8df1794a1c7044501b'
