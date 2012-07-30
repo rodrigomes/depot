cf05dcf1482fb7ec2f96cd299a21d916c31a1f09
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2012-07-30 19:17:58 -0300

    Add quantity to lines in cart.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,5 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id, :product
+  attr_accessible :cart_id, :product_id, :product, :quantity
   belongs_to :product
   belongs_to :cart
 end
