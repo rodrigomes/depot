5803e39cd342a6d789396cbaa0b3717d681ed49c
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2012-07-30 20:11:21 -0300

    Add empty cart and total price to cart view.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,9 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id, :product, :quantity
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+	product.price * quantity
+  end
+
 end
